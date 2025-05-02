c8fa1ecc07f3ab2e8a4dc80f521b45e1fcaadbab
nitin-dagur <workfordagur@gmail.com>
2025-05-02 15:23:17 +0530

    Create 206_Reverse Linked List.java

---
+++ linkedlist/206_Reverse Linked List.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+
+        while(current!=null){
+            ListNode temp = current.next;
+            current.next=prev;
+            prev=current;
+            current=temp;
+
+        }
+        return prev;
+        
+    }
+}
