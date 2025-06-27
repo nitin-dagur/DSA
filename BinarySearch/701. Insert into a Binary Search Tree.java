5699571e17bf41a2ce74b075b6bcadf36942508d
nitin-dagur <workfordagur@gmail.com>
2025-06-27 12:58:50 +0530

    Create 701. Insert into a Binary Search Tree.java

---
+++ BinarySearch/701. Insert into a Binary Search Tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            root = new TreeNode(val);
+            return root;
+        }
+        if (root.val >= val) {
+            if (root.left == null) {
+                root.left = new TreeNode(val);
+                return root;
+            }
+            insertIntoBST(root.left, val);
+        } else {
+            if (root.right == null) {
+                root.right = new TreeNode(val);
+                return root;
+            }
+            insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
